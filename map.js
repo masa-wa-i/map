664545b60386838b4af8d5b01a96ec0f0aa4e994
masa-wa-i <masa.pisces.mark8@gmail.com>
2023-01-16 20:37:07 +0900

    a

--- map.js
+++ map.js
@@ -13,6 +13,7 @@ ajax.onreadystatechange = function () {
   if (ajax.readyState === 4 && ajax.status === 200) {
     console.log(ajax.responseText);
     ajax_map = ajax.responseText.split("\r\n");
+    ajax_map = ajax_map.split("\n");
     console.log(ajax_map);
     for (var i in ajax_map) {
       map.push(JSON.parse("[" + ajax_map[i] + "]"));
